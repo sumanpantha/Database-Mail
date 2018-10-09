d9ec754d230f5aa494384f39066217f332f21c66
suman pantha <hrtpantha@gmail.com>
2018-10-09 14:23:49 +0545

    Update Excel File from DBMail.sql

--- Excel File from DBMail.sql
+++ Excel File from DBMail.sql
@@ -2,10 +2,10 @@ DECLARE
     @tab char(1) = CHAR(9)
 
 EXEC msdb.dbo.sp_send_dbmail
-    @profile_name = 'DatabaseBackup',
+    @profile_name = 'DatabaseBackup',   -- replace with your SQL Database Mail Profile 
     @recipients = 'hrtpantha@gmail.com',
     @query = 'SET NOCOUNT ON 
-              select AgentCode,AgentName,ltrim(PhoneNumber) AS PhoneNumber, Email, convert(varchar(10), CreatedDate,121)  as CreatedDate from MMESV2DBUAT..Agent' ,
+              select AgentCode,AgentName,ltrim(PhoneNumber) AS PhoneNumber, Email, convert(varchar(10), CreatedDate,121)  as CreatedDate from MMESV2DBUAT..Agent',
     @subject = 'RA results',
     @attach_query_result_as_file = 1,
     @query_attachment_filename='RATEST.CSV',
