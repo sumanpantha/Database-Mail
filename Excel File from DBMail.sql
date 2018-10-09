7db140ec3c5fa0d44c79b85435bba5ad1ebd2fe9
suman pantha <hrtpantha@gmail.com>
2018-10-09 14:22:51 +0545

    Create Excel File from DBMail.sql

---
+++ Excel File from DBMail.sql
@@ -0,0 +1,13 @@
+DECLARE
+    @tab char(1) = CHAR(9)
+
+EXEC msdb.dbo.sp_send_dbmail
+    @profile_name = 'DatabaseBackup',
+    @recipients = 'hrtpantha@gmail.com',
+    @query = 'SET NOCOUNT ON 
+              select AgentCode,AgentName,ltrim(PhoneNumber) AS PhoneNumber, Email, convert(varchar(10), CreatedDate,121)  as CreatedDate from MMESV2DBUAT..Agent' ,
+    @subject = 'RA results',
+    @attach_query_result_as_file = 1,
+    @query_attachment_filename='RATEST.CSV',
+    @query_result_separator=@tab,
+    @query_result_no_padding=1
